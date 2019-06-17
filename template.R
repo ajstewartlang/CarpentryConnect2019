c84de859d1314e2aaed1b8ca917bb52ee334dc68
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-17 12:45:35 +0100

    added slids

---
+++ template.R
@@ -0,0 +1,30 @@
+# Code along template
+
+# Main task 1 - Simulate data and run t-tests ####
+# Sample A from population with mean = 1000, sd = 20
+# Sample B from population with mean = 1010, sd = 20
+# This equates to a Cohen's d of .5 as (1010-1000)/20 so a medium effect size
+
+# First let's plot the population for each sample 
+
+# Now let's sample 50 individuals from each population
+
+# t-test to tell if the difference between Sample A and Sample B significant ####
+
+# What if we ran 1,000 simulations? ####
+# How many of those detect a signficant difference?
+
+# Main task 2 - Linear model with mtcars dataset ####
+# is mpg predicted by hp?
+
+# Test linear model against the mean as a model
+
+# Factorial ANOVA ####
+# Set cyl to factor and add ID to determine whether difference in mpg
+# differs significantly by cylinder number.
+
+# Plot
+
+# Use aov_4() to build ANOVA model
+
+# Run pairwise comparisons to tell where the difference lies
