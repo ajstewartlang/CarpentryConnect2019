73ea158a3a013995e9182142ef7ee1a884fb7e27
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-19 12:56:40 +0100

    Update template.R

--- template.R
+++ template.R
@@ -5,16 +5,18 @@
 # Sample B from population with mean = 1010, sd = 20
 # This equates to a Cohen's d of .5 as (1010-1000)/20 so a medium effect size
 
-# First let's plot the population for each sample 
+# First let's plot an approximation of the population for each sample 
 
 # Now let's sample 50 individuals from each population
 
-# t-test to tell if the difference between Sample A and Sample B significant ####
+# Plot data
+
+# Run a t-test to see if difference Sample A vs Sample B is significant ####
 
 # What if we ran 1,000 simulations? ####
 # How many of those detect a signficant difference?
 
-# Main task 2 - Linear model with mtcars dataset ####
+# Main task 2 - Linear model with the built-in mtcars dataset ####
 # is mpg predicted by hp?
 
 # Test linear model against the mean as a model
@@ -23,8 +25,8 @@
 # Set cyl to factor and add ID to determine whether difference in mpg
 # differs significantly by cylinder number.
 
-# Plot
+# Plot data
 
-# Use aov_4() to build ANOVA model
+# Use aov_4() from tehthe afex package to build ANOVA model
 
 # Run pairwise comparisons to tell where the difference lies
