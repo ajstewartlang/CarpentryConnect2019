21b0590c09fd76ce43bc4a34aa84b2ef08f2419a
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-24 10:03:03 +0100

    added to gitignore

--- completed.R
+++ completed.R
@@ -3,6 +3,7 @@ library(broom)
 library(afex)
 library(emmeans)
 
+# Simulate one data set
 set.seed(1234)
 DV <- c(round(rnorm(10000, 1000, 20)), round(rnorm(10000, 1010, 20)))
 condition <- c(rep("Condition_A", 10000), rep("Condition_B", 10000))
@@ -31,8 +32,10 @@ my_data %>%
   group_by(condition) %>%
   summarise(mean_dv = mean(DV), sd_dv = sd(DV))
 
+# Run t-test ####
 t.test(my_data[condition == "Condition_A",]$DV, my_data[condition == "Condition_B",]$DV)
 
+# Simulate 1000 datasets ####
 all_data <- NULL
 
 set.seed(1234)
@@ -43,6 +46,7 @@ for (i in 1:1000) {
   all_data <- rbind(all_data, my_data)
 }
 
+# Run 1000 t-tests
 all_results <- NULL
 for (n in 1:1000) {
   my_data_filt <- all_data %>% filter(i == n)
@@ -52,27 +56,39 @@ for (n in 1:1000) {
   
 }
 
+# How many find the difference?
 count(all_results %>% filter(p.value < .05))
 
+# Linear model ####
 # use the built in mtcars data set
 model <- lm(mpg ~ hp, data = mtcars)
 summary(model)
 
-mtcars %>% 
+renamed_mtcars <- mtcars %>%
+  rownames_to_column()
+
+filtered_mtcars <- filter(renamed_mtcars, (rowname != ("Maserati Bora")) & 
+                            (rowname != ("Lotus Europa")))
+
+model <- lm(mpg ~ hp, data = filtered_mtcars)
+summary(model)
+plot(model)
+
+filtered_mtcars %>% 
   ggplot(aes(x = hp, y = mpg)) + 
   geom_point() +
   geom_smooth(method = "lm")
 
-model_null <- lm(mpg ~ 1, data = mtcars)
+model_null <- lm(mpg ~ 1, data = filtered_mtcars)
 
-mean_intercept <- mean(mtcars$mpg)
+mean_intercept <- mean(filtered_mtcars$mpg)
 
-mtcars %>% 
+filtered_mtcars %>% 
   ggplot(aes(x = hp, y = mpg)) + 
   geom_point() +
   geom_hline(yintercept = mean_intercept, colour = "red")
 
-mtcars %>% 
+filtered_mtcars %>% 
   ggplot(aes(x = hp, y = mpg)) + 
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
@@ -80,20 +96,21 @@ mtcars %>%
 
 anova(model, model_null)
 
-mtcars %>%
+filtered_mtcars %>%
   group_by(cyl) %>%
   summarise(mean = mean(mpg), sd = sd(mpg))
 
-mtcars %>%
+filtered_mtcars %>%
   group_by(cyl) %>%
-  ggplot(aes(x = cyl, y = mpg, fill = cyl)) +
+  ggplot(aes(x = cyl, y = mpg, group = cyl, fill = cyl)) +
   geom_boxplot() +
   guides(fill = FALSE)
 
-mtcars$cyl <- as.factor(mtcars$cyl) 
-mtcars <- mtcars %>% mutate(id = row_number())
+# ANOVA ####
+filtered_mtcars$cyl <- as.factor(filtered_mtcars$cyl) 
+filtered_mtcars <- filtered_mtcars %>% mutate(id = row_number())
 
-model_ANOVA <- aov_4(mpg ~ cyl + (1 | id), data = mtcars)
+model_ANOVA <- aov_4(mpg ~ cyl + (1 | id), data = filtered_mtcars)
 nice(model_ANOVA)
 summary(model_ANOVA)
 
--- template.R
+++ template.R
@@ -1,8 +1,8 @@
 # Code along template
 
 # Main task 1 - Simulate data and run t-tests ####
-# Sample A from population with mean = 1000, sd = 20
-# Sample B from population with mean = 1010, sd = 20
+# Group A from population with mean = 1000, sd = 20
+# Group B from population with mean = 1010, sd = 20
 # This equates to a Cohen's d of .5 as (1010-1000)/20 so a medium effect size
 
 # First let's plot an approximation of the population for each sample 
@@ -11,7 +11,9 @@
 
 # Plot data
 
-# Run a t-test to see if difference Sample A vs Sample B is significant ####
+# Generate summary stats
+
+# Run a t-test to see if difference Group A vs Group B is significant ####
 
 # What if we ran 1,000 simulations? ####
 # How many of those detect a signficant difference?
@@ -19,7 +21,11 @@
 # Main task 2 - Linear model with the built-in mtcars dataset ####
 # is mpg predicted by hp?
 
-# Test linear model against the mean as a model
+# Build model and examine assumptions
+
+# Remove two outliers after (renamed_mtcars <- mtcars %>% rownames_to_column())
+
+# Test new linear model against the mean as a model
 
 # Factorial ANOVA ####
 # Set cyl to factor and add ID to determine whether difference in mpg
